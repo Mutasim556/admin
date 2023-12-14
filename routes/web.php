b2ef3d159acf853ab2072cd021f7b9e13866b83d
Md Mutasim Naib <mutasim@inceptapharma.com>
2023-12-14 12:54:44 +0600

    new commit

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('admin.auth.login');
+})->name('admin.login');
 
-Auth::routes();
+// Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
